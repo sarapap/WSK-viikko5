8b6d957f4df4ac3acdd9a2e2512a604ee453383a
sarapappila <sara.pappila@metropolia.fi>
2024-04-15 15:24:20 +0300

    Lisäystä

--- src/components/SingleView.jsx
+++ src/components/SingleView.jsx
@@ -9,18 +9,22 @@ const SingleView = props => {
     }
     return (
         <>
-            <dialog open={selectedItem ? true : false}>
+            <dialog
+                className='fixed top-0 h-dvh w-dvw bg-black bg-opacity-50 p-4 text-stone-100'
+                open={selectedItem ? true : false}>
                 <p>
                     <Button text='Close' handleClick={handleClick} />
                 </p>
                 {selectedItem && (
                     <>
                         {selectedItem.media_type.includes('video') ?
-                            <video controls>
+                            <video className='h-3/4 m-auto' controls>
                                 <source src={selectedItem.filename}
                                     type={selectedItem.media_type} />
                             </video> : (
-                                <img src={selectedItem.filename} alt={selectedItem.title} />
+                                <img
+                                    className='h-3/4 m-auto'
+                                    src={selectedItem.filename} alt={selectedItem.title} />
                             )}
                         <h2>{selectedItem.title}</h2>
                         <p>{selectedItem.description}</p>
