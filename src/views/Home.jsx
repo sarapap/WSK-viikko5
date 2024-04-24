6bfbd2d5c13fa289d55294a78b35f57478c547db
sarapappila <sara.pappila@metropolia.fi>
2024-04-24 10:11:48 +0300

    Valmis tehtävä

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,31 +1,33 @@
-import { useState } from "react";
-import MediaRow from "../components/MediaRow";
-import fetchData from "../utils/fetchData";
-import { useEffect } from "react";
+import { useEffect, useState } from 'react';
+import MediaRow from '../components/MediaRow';
+import { fetchData } from '../lib/fetchData';
 
 const Home = () => {
-    const [selectedItem, setSelectedItem] = useState(null);
+    //const [selectedItem, setSelectedItem] = useState(null);
     const [mediaArray, setMediaArray] = useState([]);
-
-    console.log(selectedItem);
 
     const getMedia = async () => {
         try {
-            const mediaResult = await fetchData(import.meta.env.VITE_MEDIA_API + '/media',
+            const mediaResult = await fetchData(
+                import.meta.env.VITE_MEDIA_API + '/media',
             );
 
-            const mediaWithUser = await Promise.all(mediaResult.map(async (mediaItem) => {
-                const userResult = await fetchData(import.meta.env.VITE_AUTH_API + '/users' + mediaItem.user_id);
-                return { ...mediaItem, username: userResult.username };
-            }));
+            const mediaWithUser = await Promise.all(
+                mediaResult.map(async (mediaItem) => {
+                    const userResult = await fetchData(
+                        import.meta.env.VITE_AUTH_API + '/users/' + mediaItem.user_id,
+                    );
+                    return { ...mediaItem, username: userResult.username };
+                }),
+            );
 
             console.log(mediaWithUser);
 
             setMediaArray(mediaWithUser);
-        }
-        catch (error) {
+        } catch (error) {
             console.error(error);
         }
+
     };
 
     useEffect(() => {
@@ -33,34 +35,34 @@ const Home = () => {
     }, []);
 
     console.log(mediaArray);
+
+    return (
+        <>
+            <h2>My Media</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Thumbnail</th>
+                        <th>Owner</th>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Created</th>
+                        <th>Size</th>
+                        <th>Type</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {mediaArray.map((item) => (
+                        <MediaRow
+                            key={item.media_id}
+                            item={item}
+                        //    setSelectedItem={setSelectedItem}
+                        />
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
 };
-
-return (
-    <>
-        <h2>My Media</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Thumbnail</th>
-                    <th>Owner</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Created</th>
-                    <th>Size</th>
-                    <th>Type</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {mediaArray.map((item) => (
-                    <MediaRow key={item.media_id}
-                        item={item}
-                        setSelectedItem={setSelectedItem} />
-                ))}
-            </tbody>
-        </table>
-    </>
-);
-
 export default Home;
-
