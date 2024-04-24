7214c193879ec7a19d327ec39f805f733869ba71
sarapappila <sara.pappila@metropolia.fi>
2024-04-24 12:06:36 +0300

    Toimii

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -1,16 +1,19 @@
-const UserData = ({ user }) => {
-    const { user } = useUserContext();
+import { useUserContext } from "../contexts/UserContext";
+
+const UserData = () => {
+    const { user } = useUserContext()
+
     if (!user) {
         return null;
     }
 
     return (
         <>
-            <p>Käyttäjätunnus: {user.username}</p>
-            <p>Email: {user.email}</p>
-            <p>Luotu: {new Date(user.created_at).toLocaleString('fi-FI')}</p>
+            <p>Käyttäjätunnus: {user.username} </p>
+            <p>email: {user.email} </p>
+            <p>luotu: {new Date(user.created_at).toLocaleString('fi-FI')}</p>
         </>
-    );
+    )
 }
 
-export default UserData;
+export default UserData
